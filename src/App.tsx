d9edaed362505b7561c3e7b268d63313ff81c5e2
Lubos Belak <belak.lubos@gmail.com>
2021-01-30 16:37:00 +0100

    :heavy_plus_sign: Add and setup  `wouter`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import { Link, Route } from 'wouter';
 import './App.css';
 
 function App() {
-  return <main />;
+  return (
+    <div>
+      <nav>
+        <Link href="/lessons/03-basic-scene">Basic Scene</Link>
+        <Link href="/lessons/05-transforms-objects">Transforms Objects</Link>
+        <Link href="/lessons/06-animations">Animations</Link>
+      </nav>
+
+      <main>
+        <Route path="/lessons/03-basic-scene">Basic Scene</Route>
+        <Route path="/lessons/05-transforms-objects">Transforms Objects</Route>
+        <Route path="/lessons/06-animations">Animations</Route>
+      </main>
+    </div>
+  );
 }
 
 export default App;
