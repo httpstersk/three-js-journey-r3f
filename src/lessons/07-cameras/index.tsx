404bf1c25e6dc0a956e507a76aec28bfc47958fe
Lubos Belak <belak.lubos@gmail.com>
2021-02-02 21:06:28 +0100

    :sparkles: 07-cameras

--- src/lessons/07-cameras/index.tsx
+++ src/lessons/07-cameras/index.tsx
@@ -1,11 +1,53 @@
+import { useEffect, useRef } from 'react';
+import { useFrame, useThree } from 'react-three-fiber';
+import { PerspectiveCamera } from '@react-three/drei';
+import { PerspectiveCamera as PerspectiveCameraType, Vector2 } from 'three';
 import Cube from 'components/Cube';
-import PerspectiveCamera from './PerspectiveCamera';
 
 export default function Scene() {
+  const camera = useRef<PerspectiveCameraType>();
+  const cursor = useRef(new Vector2(0, 0));
+  const { setDefaultCamera } = useThree();
+
+  const onMouseMove = ({ clientX, clientY }: any) => {
+    const { innerHeight, innerWidth } = window;
+    cursor.current.x = clientX / innerWidth;
+    cursor.current.y = clientY / innerHeight;
+  };
+
+  useEffect(() => {
+    if (camera.current) {
+      setDefaultCamera(camera.current);
+    }
+  }, [camera, setDefaultCamera]);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', onMouseMove);
+    return () => window.removeEventListener('mousemove', onMouseMove);
+  });
+
+  useFrame(() => {
+    if (camera.current) {
+      const { x, y } = cursor.current;
+      camera.current.position.x = Math.sin(x * Math.PI * 2) * 3;
+      camera.current.position.z = Math.cos(x * Math.PI * 2) * 3;
+      camera.current.position.y = y * 3;
+    }
+  });
+
   return (
-    <group>
+    <>
       <Cube color={0xff0000} />
-      <PerspectiveCamera />
-    </group>
+
+      <PerspectiveCamera
+        aspect={2}
+        far={100}
+        fov={75}
+        makeDefault
+        near={0.1}
+        position={[2, 2, 2]}
+        ref={camera}
+      />
+    </>
   );
 }
