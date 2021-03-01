f0597f5d4b497ee4dbed7b1b2d1c08ee1d9916c2
Lubos Belak <belak.lubos@gmail.com>
2021-03-01 20:51:26 +0100

    :sparkles: <Fog>

--- src/lessons/16-haunted-house/index.tsx
+++ src/lessons/16-haunted-house/index.tsx
@@ -1,10 +1,25 @@
 import { PerspectiveCamera } from '@react-three/drei';
+import { useEffect } from 'react';
+import { useThree } from 'react-three-fiber';
+import { Fog } from 'three';
 import Floor from './components/Floor';
 import Graves from './components/Graves';
 import House from './components/House';
 import { AmbientLight, DoorLight, MoonLight } from './lights';
 
+const FOG_COLOR = 0x262837;
+
 export default function Scene() {
+  const { scene, gl } = useThree();
+
+  useEffect(() => {
+    scene.fog = new Fog(FOG_COLOR, 1, 15);
+  }, [scene]);
+
+  useEffect(() => {
+    gl.setClearColor(FOG_COLOR);
+  }, [gl]);
+
   return (
     <>
       <AmbientLight color={0xb9d5ff} intensity={0.12} />
