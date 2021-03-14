3552cfa13b1841c0b9439646fa5c83196278b7b2
Lubos Belak <belak.lubos@gmail.com>
2021-03-14 12:37:53 +0100

    :sparkles: Animate the Particles

--- src/lessons/17-particles/index.tsx
+++ src/lessons/17-particles/index.tsx
@@ -1,5 +1,6 @@
 import { useTexture } from '@react-three/drei';
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { useMemo } from 'react';
+import { useFrame } from 'react-three-fiber';
 import {
   AdditiveBlending,
   BufferAttribute,
@@ -11,9 +12,10 @@ interface IProps {
   size: number;
 }
 
-const Particles: React.FC<IProps> = ({ size, ...rest }) => {
+const Particles: React.FC<IProps> = ({ size }) => {
   const texture = useTexture('/textures/particles/1.png') as Texture;
   const COUNT = 5000;
+  const VERTICES = 3;
   const VERTEX_SIZE = 3;
   const length = COUNT * VERTEX_SIZE;
 
@@ -25,36 +27,53 @@ const Particles: React.FC<IProps> = ({ size, ...rest }) => {
       (_, i) => (positions[i] = (Math.random() - 0.5) * 10)
     );
 
+    const colors = new Float32Array(length);
+    [...Array(length)].map((_, i) => (colors[i] = Math.random()));
+
     geometry.setAttribute(
       'position',
       new BufferAttribute(positions, VERTEX_SIZE)
     );
 
+    geometry.setAttribute('color', new BufferAttribute(colors, VERTEX_SIZE));
+
     return geometry;
   }, [length]);
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+
+    [...Array(length)].map((_, i) => {
+      const i3 = i * VERTICES;
+      let x = geometry.attributes.position.array[i3];
+      // @ts-ignore
+      return (geometry.attributes.position.array[i3 + 1] = Math.sin(
+        elapsedTime + x
+      ));
+    });
+
+    geometry.attributes.position.needsUpdate = true;
+  });
 
   return (
     <points>
       <primitive attach="geometry" object={geometry} />
       <pointsMaterial
         alphaMap={texture}
+        // alphaTest={0.001}
         blending={AdditiveBlending}
-        color={0xff88cc}
+        // color={0xff88cc}
+        // depthTest={false}
         depthWrite={false}
         size={size}
         sizeAttenuation={true}
         transparent
+        vertexColors
       />
     </points>
   );
 };
 
 export default function Scene() {
-  return (
-    <mesh>
-      <group>
-        <Particles size={0.1} />
-      </group>
-    </mesh>
-  );
+  return <Particles size={0.1} />;
 }
