ffc60abc03cd637022e161e42f91ff045d9e6267
Lubos Belak <belak.lubos@gmail.com>
2021-03-21 16:58:25 +0100

    :sparkles: Use controls for  `radius`

--- src/lessons/18-galaxy-generator/index.tsx
+++ src/lessons/18-galaxy-generator/index.tsx
@@ -9,17 +9,16 @@ const Galaxy: React.FC<IProps> = () => {
   const VERTICES = 3;
   const VERTEX_SIZE = 3;
 
-  const { count, size } = useControls({
+  const { count, radius, size } = useControls({
     count: {
       min: Math.floor(Math.sqrt(COUNT)),
       max: COUNT * 2,
       step: 1,
       value: COUNT,
     },
-    size: { value: 0.01, min: 0.005, max: 0.1, step: 0.001 },
+    radius: { min: 0.01, max: 20, step: 0.01, value: 5 },
+    size: { min: 0.005, max: 0.1, step: 0.001, value: 0.01 },
   });
-
-  console.log(count);
 
   const geometry = useMemo(() => {
     let geometry = new BufferGeometry();
@@ -30,10 +29,10 @@ const Galaxy: React.FC<IProps> = () => {
     const positions = new Float32Array(count * VERTEX_SIZE);
     [...Array(count * VERTEX_SIZE)].map((_, i) => {
       const i3 = i * VERTICES;
-      positions[i3] = (Math.random() - 0.5) * 3;
-      positions[i3 + 1] = (Math.random() - 0.5) * 3;
-      positions[i3 + 2] = (Math.random() - 0.5) * 3;
-
+      const r = Math.random() * radius;
+      positions[i3] = r;
+      positions[i3 + 1] = 0;
+      positions[i3 + 2] = 0;
       return positions;
     });
 
@@ -45,7 +44,7 @@ const Galaxy: React.FC<IProps> = () => {
     );
 
     return geometry;
-  }, [count]);
+  }, [count, radius]);
 
   return (
     <points>
