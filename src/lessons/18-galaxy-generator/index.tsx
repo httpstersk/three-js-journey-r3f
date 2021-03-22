87bbd94e7ce4bec7c86c1607cc433b7ccd641470
Lubos Belak <belak.lubos@gmail.com>
2021-03-22 20:26:00 +0100

    :sparkles: Use controls for the `randomness`

--- src/lessons/18-galaxy-generator/index.tsx
+++ src/lessons/18-galaxy-generator/index.tsx
@@ -9,15 +9,25 @@ const Galaxy: React.FC<IProps> = () => {
   const VERTICES = 3;
   const VERTEX_SIZE = 3;
 
-  const { branches, count, radius, size, spin } = useControls({
+  const {
+    branches,
+    count,
+    radius,
+    randomness,
+    randomnessPower,
+    size,
+    spin,
+  } = useControls({
     branches: { min: 2, max: 20, step: 1, value: 5 },
     count: {
       min: Math.floor(Math.sqrt(COUNT)),
-      max: COUNT * 2,
+      max: 100_000,
       step: 1,
       value: COUNT,
     },
     radius: { min: 0.01, max: 20, step: 0.01, value: 5 },
+    randomness: { min: 0, max: 2, step: 0.001, value: 0.2 },
+    randomnessPower: { min: 1, max: 10, step: 0.001, value: 3 },
     size: { min: 0.005, max: 0.1, step: 0.001, value: 0.01 },
     spin: { min: -5, max: 5, step: 0.001, value: 1 },
   });
@@ -35,9 +45,19 @@ const Galaxy: React.FC<IProps> = () => {
       const branchesAngle = ((i % branches) / branches) * Math.PI * 2;
       const spinAngle = r * spin;
 
-      positions[i3] = Math.cos(branchesAngle + spinAngle) * r;
-      positions[i3 + 1] = 0;
-      positions[i3 + 2] = Math.sin(branchesAngle + spinAngle) * r;
+      const getRandom = () =>
+        Math.pow(Math.random(), randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1) *
+        randomness *
+        r;
+
+      const randomX = getRandom();
+      const randomY = getRandom();
+      const randomZ = getRandom();
+
+      positions[i3] = Math.cos(branchesAngle + spinAngle) * r + randomX;
+      positions[i3 + 1] = randomY;
+      positions[i3 + 2] = Math.sin(branchesAngle + spinAngle) * r + randomZ;
 
       return positions;
     });
@@ -50,7 +70,7 @@ const Galaxy: React.FC<IProps> = () => {
     );
 
     return geometry;
-  }, [branches, count, radius, spin]);
+  }, [branches, count, radius, randomness, randomnessPower, spin]);
 
   return (
     <points>
