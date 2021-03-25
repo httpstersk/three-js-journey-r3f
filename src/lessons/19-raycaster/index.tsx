2629c888dd87ee9d543ee93ae3af39540215d2cf
Lubos Belak <belak.lubos@gmail.com>
2021-03-25 13:30:33 +0100

    :sparkles: Shoot the Raycaster

--- src/lessons/19-raycaster/index.tsx
+++ src/lessons/19-raycaster/index.tsx
@@ -1,48 +1,67 @@
 import { Sphere } from '@react-three/drei';
 import { useRef } from 'react';
-import { useFrame, useResource } from 'react-three-fiber';
-import { Group, Mesh, MeshStandardMaterial } from 'three';
+import { useFrame, useResource, useThree } from 'react-three-fiber';
+import { Group, Mesh, MeshStandardMaterial, Vector3 } from 'three';
 
 export default function Scene() {
   const groupRef = useRef<Group>();
-  const meshRef = useRef<Mesh>();
+  const object1Ref = useRef<Mesh>();
+  const object2Ref = useRef<Mesh>();
+  const object3Ref = useRef<Mesh>();
   const material = useResource<MeshStandardMaterial>();
+  const { raycaster } = useThree();
 
-  useFrame((_, delta) => {
-    if (groupRef.current && meshRef.current) {
-      const groupRotation = groupRef.current.rotation;
-      const meshRotation = meshRef.current.rotation;
-      meshRotation.x = groupRotation.x = groupRotation.y += 0.15 * delta;
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    const rayOrigin = new Vector3(-3, 0, 0);
+    const rayDirection = new Vector3(10, 0, 0);
+
+    rayDirection.normalize();
+    raycaster.set(rayOrigin, rayDirection);
+
+    if (object1Ref.current && object2Ref.current && object3Ref.current) {
+      const intersects = raycaster.intersectObjects([
+        object1Ref.current,
+        object2Ref.current,
+        object3Ref.current,
+      ]);
+
+      console.log(intersects.length);
+
+      object1Ref.current.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+      object2Ref.current.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+      object3Ref.current.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
     }
   });
 
   return (
-    <>
-      <mesh>
-        <meshBasicMaterial color={0xff0000} ref={material} />
+    <mesh>
+      <meshBasicMaterial color={0xff0000} ref={material} />
 
-        {material.current && (
-          <group ref={groupRef}>
-            <Sphere
-              args={[0.5, 16, 16]}
-              material={material.current}
-              position-x={-2}
-            />
+      {material.current && (
+        <group ref={groupRef}>
+          <Sphere
+            args={[0.5, 16, 16]}
+            material={material.current}
+            position-x={-2}
+            ref={object1Ref}
+          />
 
-            <Sphere
-              args={[0.5, 16, 16]}
-              material={material.current}
-              position-x={0}
-            />
+          <Sphere
+            args={[0.5, 16, 16]}
+            material={material.current}
+            position-x={0}
+            ref={object2Ref}
+          />
 
-            <Sphere
-              args={[0.5, 16, 16]}
-              material={material.current}
-              position-x={2}
-            />
-          </group>
-        )}
-      </mesh>
-    </>
+          <Sphere
+            args={[0.5, 16, 16]}
+            material={material.current}
+            position-x={2}
+            ref={object3Ref}
+          />
+        </group>
+      )}
+    </mesh>
   );
 }
