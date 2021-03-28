83ec98429d19d05a11ca3b739cd9bbb19c4df902
Lubos Belak <belak.lubos@gmail.com>
2021-03-28 17:45:08 +0200

    :recycle: `setMaterialColor` function

--- src/lessons/19-raycaster/index.tsx
+++ src/lessons/19-raycaster/index.tsx
@@ -1,43 +1,45 @@
 import { Sphere } from '@react-three/drei';
 import { useRef } from 'react';
-import { useFrame, useResource, useThree } from 'react-three-fiber';
-import { Group, Mesh, MeshBasicMaterial, Vector3 } from 'three';
+import { useFrame, useThree } from 'react-three-fiber';
+import { Group, Mesh, MeshBasicMaterial } from 'three';
+
+const INITIAL_OBJECT_COLOR = 0x0000ff;
+const HOVER_OBJECT_COLOR = 0xff0000;
+
+const setMaterialColor = (object: any, color = 0xffffff) => {
+  if (object.material instanceof MeshBasicMaterial) {
+    return object.material.color.set(color);
+  }
+};
 
 export default function Scene() {
   const groupRef = useRef<Group>();
   const object1Ref = useRef<Mesh>();
   const object2Ref = useRef<Mesh>();
   const object3Ref = useRef<Mesh>();
-  const material = useResource<MeshBasicMaterial>();
   const { camera, raycaster } = useThree();
-  const rayDirection = new Vector3(10, 0, 0);
-  rayDirection.normalize();
 
   useFrame(({ clock, mouse }) => {
     const elapsedTime = clock.elapsedTime;
     raycaster.setFromCamera(mouse, camera);
 
     if (object1Ref.current && object2Ref.current && object3Ref.current) {
+      object1Ref.current.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+      object2Ref.current.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+      object3Ref.current.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
       const objectsToTest = [
         object1Ref.current,
         object2Ref.current,
         object3Ref.current,
       ];
 
-      object1Ref.current.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
-      object2Ref.current.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
-      object3Ref.current.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
-
       const intersects = raycaster.intersectObjects(objectsToTest);
 
       if (intersects.length) {
-        intersects.map((intersect) => {
-          if (intersect.object instanceof Mesh) {
-            return intersect.object.material.color.set(0x0000ff);
-          }
-
-          return intersect;
-        });
+        objectsToTest.map((object) =>
+          setMaterialColor(object, INITIAL_OBJECT_COLOR)
+        );
 
         objectsToTest
           .filter((object) =>
@@ -45,45 +47,26 @@ export default function Scene() {
               return intersect.object === object;
             })
           )
-          .map((object) => {
-            if (object.material instanceof MeshBasicMaterial) {
-              return object.material.color.set(0xff0000);
-            }
-
-            return object;
-          });
+          .map((object) => setMaterialColor(object, HOVER_OBJECT_COLOR));
       }
     }
   });
 
   return (
     <mesh>
-      <meshBasicMaterial color={0xff0000} ref={material} />
+      <group ref={groupRef}>
+        <Sphere args={[0.5, 16, 16]} position-x={-2} ref={object1Ref}>
+          <meshBasicMaterial attach="material" />
+        </Sphere>
 
-      {material.current && (
-        <group ref={groupRef}>
-          <Sphere
-            args={[0.5, 16, 16]}
-            material={material.current}
-            position-x={-2}
-            ref={object1Ref}
-          />
+        <Sphere args={[0.5, 16, 16]} position-x={0} ref={object2Ref}>
+          <meshBasicMaterial attach="material" />
+        </Sphere>
 
-          <Sphere
-            args={[0.5, 16, 16]}
-            material={material.current}
-            position-x={0}
-            ref={object2Ref}
-          />
-
-          <Sphere
-            args={[0.5, 16, 16]}
-            material={material.current}
-            position-x={2}
-            ref={object3Ref}
-          />
-        </group>
-      )}
+        <Sphere args={[0.5, 16, 16]} position-x={2} ref={object3Ref}>
+          <meshBasicMaterial attach="material" />
+        </Sphere>
+      </group>
     </mesh>
   );
 }
